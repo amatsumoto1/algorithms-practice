5331fc28cfd5391cbb63f8809cbb35cb473cc9b1
amatsumoto1 <amatsumoto95@gmail.com>
2021-02-05 22:58:21 -0700

    Corrected CMakeList file google test directories

--- test/data_structures/LinkedListTest.cpp
+++ test/data_structures/LinkedListTest.cpp
@@ -1,1 +1,12 @@
 #include "gtest/gtest.h"
+
+#include "LinkedList.h"
+
+namespace DataStructures {
+
+TEST(LinkedListTests, constructor) 
+    LinkedList<int> list;
+
+    EXPECT_EQ(list.GetSize(), 0);
+)
+}
