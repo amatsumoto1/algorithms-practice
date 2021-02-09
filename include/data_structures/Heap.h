0b474ed625d68b3b860aacae4a0e206565e832ad
amatsumoto1 <amatsumoto95@gmail.com>
2021-02-09 13:12:08 -0700

    Initial heap skeleton code

--- include/data_structures/Heap.h
+++ include/data_structures/Heap.h
@@ -1,4 +1,28 @@
 #ifndef HEAP_H
 #define HEAP_H
 
+namespace DataStructures {
+
+template <typename T>
+class Heap {
+public:
+    Heap(bool isMax=true);
+    ~Heap();
+
+    int GetSize() const;
+    T Peek();
+    void Push(T value);
+    T Pop();
+
+private:
+    void BubbleUp(int index);
+    void BubbleDown(int index);
+
+    T* array_;
+    int size_;
+    int capacity_;
+};
+
+}
+
 #endif
