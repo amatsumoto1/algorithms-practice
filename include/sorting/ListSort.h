33445a6ca2b4bfe50a8d4f5c1017f125790fb003
amatsumoto1 <amatsumoto95@gmail.com>
2021-02-10 23:25:59 -0700

    Added initial sorting implementation

---
+++ include/sorting/ListSort.h
@@ -0,0 +1,14 @@
+#ifndef LIST_SORT_H
+#define LIST_SORT_H
+
+namespace Sorting {
+
+    void SelectionSort(int* array, int n);
+    void InsertionSort(int* array, int n);
+    void BubbleSort(int* array, int n);
+    void MergeSort(int* array, int n);
+    void QuickSort(int* array, int n);
+    //void HeapSort(int* array, int n);
+}
+
+#endif
---
+++ test/sorting/ListSortTest.cpp
@@ -0,0 +1,61 @@
+#include "gtest/gtest.h"
+
+#include "ListSort.h"
+
+namespace Sorting {
+
+    TEST(ListSortingTests, SelectionSort) {
+        int array[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = 9-i;
+        }
+        SelectionSort(array, 10);
+        for (int i = 0; i < 10; i++) {
+            EXPECT_EQ(array[i], i);
+        }
+    }
+
+    TEST(ListSortingTests, InsertionSort) {
+        int array[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = 9-i;
+        }
+        InsertionSort(array, 10);
+        for (int i = 0; i < 10; i++) {
+            EXPECT_EQ(array[i], i);
+        }
+    }
+
+    TEST(ListSortingTests, BubbleSort) {
+        int array[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = 9-i;
+        }
+        BubbleSort(array, 10);
+        for (int i = 0; i < 10; i++) {
+            EXPECT_EQ(array[i], i);
+        }
+    }
+
+    TEST(ListSortingTests, MergeSort) {
+        int array[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = 9-i;
+        }
+        MergeSort(array, 10);
+        for (int i = 0; i < 10; i++) {
+            EXPECT_EQ(array[i], i);
+        }
+    }
+
+    TEST(ListSortingTests, QuickSort) {
+        int array[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = 9-i;
+        }
+        QuickSort(array, 10);
+        for (int i = 0; i < 10; i++) {
+            EXPECT_EQ(array[i], i);
+        }
+    }
+}
